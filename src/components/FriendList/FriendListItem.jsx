1a5727d2a4a87762dd48e714ab22f703ac2eb1b1
denis-cactus <den.waves@gmail.com>
2025-03-31 19:55:45 +0300

    push to deploy

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,8 +3,8 @@ import styles from './FriendList.module.css';
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <div className={styles.friendCard}>
-      <img src={avatar} alt={`${name}'s avatar`} width="48" />
-      <p>{name}</p>
+      <img src={avatar} alt={`${name}'s avatar`} width="64" />
+      <p className={styles.friendName}>{name}</p>
       <p className={isOnline ? styles.online : styles.offline}>
         {isOnline ? 'Online' : 'Offline'}
       </p>
